083d04e1c7912957d5ad93c2d4f79aa7affcbf4f
igniokaz <21201390+igniokaz@users.noreply.github.com>
2025-02-04 20:50:31 +0100

    Dbt project init (#6)
    * dbt project init
    
    * init my dbt project
    
    * dbt build first attempt
    
    * work
    
    * sync2
    
    * x
    
    * packages upgrade
    
    * docs
    
    * fixing model name
    
    * removing old model
    
    * testing CI check job
    
    ---------
    
    Co-authored-by: ignas.data.eng@gmail.com <ignas.data.eng@gmail.com>

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
@@ -54,7 +54,7 @@ select
     trips_unioned.total_amount, 
     trips_unioned.payment_type, 
     trips_unioned.payment_type_description
-from trips_unioned
+from trips_unioned 
 inner join dim_zones as pickup_zone 
 on trips_unioned.pickup_locationid = pickup_zone.locationid
 inner join dim_zones as dropoff_zone 
