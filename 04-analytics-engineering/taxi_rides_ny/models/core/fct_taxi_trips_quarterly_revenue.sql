808a31681d8ec7ec13ea3dcb128c1e0f144af83b
ignas.data.eng.1@gmail.com <ignas.data.eng.1@gmail.com>
2025-02-27 08:52:32 +0000

    formatting core model code

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -1,15 +1,9 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(materialized="table") }}
 
-with trips_data as (
-    select * from {{ ref('fact_trips') }}
-)
-    select 
-        service_type,
-        revenue_year_quarter,
-        sum(total_amount) as revenue_monthly_total_amount
-    from trips_data
-    group by 1,2
+with trips_data as (select * from {{ ref("fact_trips") }})
+select
+    service_type,
+    revenue_year_quarter,
+    sum(total_amount) as revenue_monthly_total_amount
+from trips_data
+group by 1, 2
